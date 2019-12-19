fd3c7a76125fb0838899491c521e7e8f2fd1b11d
mgroff1 <mick.groff@gmail.com>
2019-12-19 16:12:31 -0700

    --

--- onboard/src/MySelect.js
+++ onboard/src/MySelect.js
@@ -4,8 +4,7 @@ import { StyledLabel, StyledSelect, StyledErrorMessage } from "./StyledSelect";
 
 
 export const MySelect = ({ label, ...props }) => {
-    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-    // which we can spread on <input> and alse replace ErrorMessage entirely.
+
     const [field, meta] = useField(props);
     return (<>
         <StyledLabel htmlFor={props.id || props.name}>{label}</StyledLabel>
--- onboard/src/MyTextInput.js
+++ onboard/src/MyTextInput.js
@@ -3,8 +3,7 @@ import { useField } from "formik";
 
 
 export const MyTextInput = ({ label, ...props }) => {
-    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-    // which we can spread on <input> and alse replace ErrorMessage entirely.
+
     const [field, meta] = useField(props);
     return (<>
         <label htmlFor={props.id || props.name}>{label}</label>
